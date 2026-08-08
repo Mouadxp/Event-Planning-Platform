40a8b3ddeca70141ce9d68327066bf542b100fb7
mouad <elba0072@algonquinlive.com>
2026-08-08 14:37:12 -0400

    DTOs added

--- obj/Debug/net10.0/Event-Planning-Platform.AssemblyInfo.cs
+++ obj/Debug/net10.0/Event-Planning-Platform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Event-Planning-Platform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3692e3228e110a494fc89a395064a6fbc2ffd8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47e4c40a368d432827bf7f8e175b2dc993f788f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Event-Planning-Platform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Event-Planning-Platform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
